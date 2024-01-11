553c821b98450d08b160405905aa22267dd5e270
gaspardCh <152152945+gaspardCh@users.noreply.github.com>
2024-01-11 21:57:08 +0200

    Update train.py

--- Dataset_2/train.py
+++ Dataset_2/train.py
@@ -8,18 +8,3 @@ import random
 #model.to('cuda')
 # Use the model
 # model.train(data="config.yaml", epochs=15)  # train the model
-
-image_dir = r"C:\Users\gaspa\Documents\ENSEA\SIA\ProjetSIA_Chapron\ImageRecognition-main\cropandweed-dataset-main" \
-            r"\cropandweed-dataset-main\data\images"
-image_list = os.listdir(image_dir)
-model = YOLO(r"C:\Users\gaspa\Documents\ENSEA\SIA\ProjetSIA_Chapron\ImageRecognition-main\cropandweed-dataset-main"
-             r"\cropandweed-dataset-main\runs\detect\train3\weights\best.pt")
-n = 3
-
-for k in range(n):
-    image = random.choice(image_list)
-    model.predict(
-        source=os.path.join(image_dir, image),
-        save=True, show=True)
-
-
