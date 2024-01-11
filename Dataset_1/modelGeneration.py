a0788e63739c3c79d735f3a48fd582d99e39e46f
gaspardCh <152152945+gaspardCh@users.noreply.github.com>
2024-01-11 20:04:13 +0200

    Add files via upload

---
+++ Dataset_1/modelGeneration.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+
+# Définir des hyper-paramètres
+batch_size = 45  # Nombre d'image avant actualisation des poids
+epochs = 2  # Nombre d'itérations
+IMG_HEIGHT = 512
+IMG_WIDTH = 512
+
+# Définition des dossier d'image
+train_dir = "dataset/train"
+validation_dir = "dataset/validation"
+
+# Prétraiter les images
+train_image_generator = ImageDataGenerator(rescale=1. / 255,
+                                           rotation_range=45,
+                                           width_shift_range=.15,
+                                           height_shift_range=.15,
+                                           horizontal_flip=True,
+                                           zoom_range=0.5,
+                                           fill_mode='nearest')
+validation_image_generator = ImageDataGenerator(rescale=1. / 255)
+
+train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size,
+                                                           directory=train_dir,
+                                                           shuffle=True,
+                                                           target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                           class_mode='categorical')
+
+val_data_gen = validation_image_generator.flow_from_directory(batch_size=batch_size,
+                                                              directory=validation_dir,
+                                                              target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                              class_mode='categorical')
+
+# Créer le modèle CNN
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(4, activation='softmax')
+])
+
+# Compiler le modèle
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Entraîner le modèle
+history = model.fit(
+    train_data_gen,
+    steps_per_epoch=train_data_gen.samples // batch_size,
+    validation_data=val_data_gen,
+    validation_steps=val_data_gen.samples // batch_size,
+    epochs=epochs
+)
+
+# Sauvegarder le modèle
+model.save("model_100epoch.h5")
---
+++ Dataset_1/showPrediction.py
@@ -0,0 +1,107 @@
+import tkinter as tk
+from tkinter import filedialog
+import customtkinter as CTk
+from PIL import Image, ImageTk
+import os
+import numpy as np
+from tensorflow import keras
+from keras.preprocessing.image import load_img, img_to_array
+import random as rd
+
+# Le chemin d'accès au dossier
+folder_path = "dataset/test/"
+images_list = os.listdir(folder_path)
+
+# Chemin d'accès au modèle
+model_path = 'model_150_epochs.h5'
+
+IMG_HEIGHT = 512
+IMG_WIDTH = 512
+
+fenetre = CTk.CTk()
+width = 1400
+height = 700
+screen_width = fenetre.winfo_screenwidth()  # Width of the screen
+screen_height = fenetre.winfo_screenheight()  # Height of the screen
+
+# Calculate Starting X and Y coordinates for Window
+x = (screen_width / 2) - (width / 2)
+y = (screen_height / 2) - (height / 2)
+fenetre.geometry('%dx%d+%d+%d' % (width, height, x, y))
+fenetre.resizable(width=0, height=0)
+
+# Définir le titre de la fenêtre
+fenetre.title("Image Recognition")
+
+# Charger l'image avec la bibliothèque PIL
+image = Image.open("logo_recognition.png")
+width, height = image.size
+ratio = 600 / height
+newSize = (int(width * ratio), int(height * ratio))
+image = image.resize(newSize)
+
+# Ajouter une étiquette pour afficher l'image
+label_image = tk.Label(fenetre)
+label_image.pack()
+
+# Afficher l'image
+photo = ImageTk.PhotoImage(image)
+label_image.configure(image=photo)
+
+# Ajouter une étiquette pour afficher du texte
+label_texte = CTk.CTkLabel(fenetre, text="Click on the button to predict the species of a weed.", font=('Arial', 20))
+label_texte.pack()
+# Charger le modèle sauvegardé
+model = keras.models.load_model(model_path)
+
+
+# Définir une fonction à appeler lorsqu'on clique sur le bouton
+def click_prediction():
+    ##Prediction
+    # Selection d'un fichier aléatoire
+    index = rd.randint(0, len(images_list) - 1)
+    # Le chemin complet du fichier
+    file_path = os.path.join(folder_path, images_list[index])
+    # Charger l'image que vous souhaitez prédire
+    image = load_img(file_path, target_size=(IMG_HEIGHT, IMG_WIDTH))
+
+    # Convertir l'image en tableau numpy
+    image_array = img_to_array(image)
+
+    # Étendre les dimensions du tableau numpy pour qu'il ait la forme (1, 224, 224, 3)
+    image_array = image_array.reshape((1,) + image_array.shape)
+
+    # Prétraiter les données
+    image_array = image_array / 255.0
+
+    # Faire une prédiction sur l'image
+    prediction = model.predict(image_array)
+    index_max_prediction = np.argmax(prediction)
+    class_prediction = "Cocklebur" if index_max_prediction == 0 else (
+        "Foxtail" if index_max_prediction == 1 else ("Pigweed" if index_max_prediction == 2 else ("Ragweed")))
+
+    # Afficher la prédiction
+    my_text = "label : " + images_list[index] + " | prediction : " + class_prediction + " | probability : " + str(
+        np.around(prediction[0][index_max_prediction] * 100, decimals=2)) + " %."
+    label_texte.configure(text=my_text)
+    ##Affichage de l'image
+    global photo
+    # Charger la nouvelle image
+    newImage = Image.open(file_path)
+    width, height = newImage.size
+    ratio = 600 / height
+    newSize = (int(width * ratio), int(height * ratio))
+    newImage = newImage.resize(newSize)
+    # Mettre à jour la variable photo avec la nouvelle image
+    photo = ImageTk.PhotoImage(newImage)
+    # Mettre à jour l'image affichée
+    label_image.configure(image=photo)
+
+
+# Ajouter un bouton cliquable
+bouton = CTk.CTkButton(fenetre, text="Predict random image", command=click_prediction)
+bouton.pack()
+
+# Lancer la boucle principale de la fenêtre
+click_prediction()
+fenetre.mainloop()
